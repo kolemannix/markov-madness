1e51d811c0051137bbbd7cc4a1d3a5d43ea83698
Koleman Nix <kolemannix@gmail.com>
2014-03-01 15:59:23 -0500

    complete index-frequencies function, write some tests
    Signed-off-by: Koleman Nix <kolemannix@gmail.com>

--- markov-clj/src/markov_clj/markov.clj
+++ markov-clj/src/markov_clj/markov.clj
@@ -1,51 +1,33 @@
 (ns markov-clj.markov)
 
-(def corpus
-  (re-seq #"\w+" (slurp "corpus.txt")))
-
-(def baby-corpus (re-seq #"\w+" (slurp "corpus2.txt")))
-
-(defn- generate-tuples [n corpus]
+(defn generate-tuples [n corpus]
   (partition n 1 corpus))
-
-(println (generate-tuples 3 corpus))
 
 (defn index-tuple [tuple]
   (let [key (vec (drop-last tuple))
         value (last tuple)]
     [key value]))
 
-(defn build-database [corpus n] (->> corpus
-                                     (generate-tuples n ,,)
-                                     (map index-tuple ,,)
-                                     (into {}) ,,))
 
-
-(->> corpus
-     (generate-tuples 2 ,,)
-     (map index-tuple ,,)
-     (reduce index-frequencies ,,))
-
-(defn- index-frequencies [database [key val]]
+(defn index-frequencies [database [key val]]
   (if-let [val-at-key (database key)]
     ;; Prefix is in database
-    (if (contains? val-at-key val)
-      (assoc val-at-key val (inc (val-at-key val)))
-      (assoc val-at-key val 1))
+    (let [new-val-at-key (if (contains? val-at-key val)
+                           (assoc val-at-key val (inc (val-at-key val)))
+                           (assoc val-at-key val 1))]
+      (assoc database key new-val-at-key))
     ;; Prefix is not in database
     (assoc database key {val 1})))
 
-(println (build-database corpus 2))
+(defn build-database [corpus n] (->> corpus
+                                     (generate-tuples n ,,)
+                                     (map index-tuple ,,)
+                                     (reduce index-frequencies {} ,,)))
+
+
 ;; TODO map-filtered somethingwhatsit
-(defn- create-sentence [seed database]
-  (letfn (iterate-word [sentence]
-           (let [next-key (take-last 2 sentence)]
-             )))
-  (vals (filter (fn [[k v]] (= k seed)) database)))
+(defn create-sentence [seed database]
+  )
 
-(defn- lookup [key database]
-  (vals (filter (fn [[k v]] (= k key)) database)))
-(lookup ["The"] (build-database baby-corpus 2))
-
-(println (build-database baby-corpus 3))
-(println (create-sentence "The car" (build-database baby-corpus 3)))
+(defn lookup [key database]
+  (database key))
--- markov-clj/test/markov_clj/markov_test.clj
+++ markov-clj/test/markov_clj/markov_test.clj
@@ -1,6 +1,47 @@
 (ns markov-clj.markov-test
-  (:require [midje :refer :all]
-            [markov-clj.core :refer :all]))
+  (:require [midje.sweet :refer :all]
+            ;; [midje.util :refer [testable-privates]]
+            [markov-clj.markov :refer :all]))
 
-(fact "Works"
-  (+ 1 1) => 2)
+
+(def corpus (re-seq #"\w+" (slurp "resources/corpus.txt")))
+
+(def baby-corpus (re-seq #"\w+" (slurp "resources/corpus2.txt")))
+
+(println baby-corpus)
+(build-database baby-corpus 2)
+
+(fact "Database is generated properly for small corpus"
+  (build-database baby-corpus 3) => {["off" "The"] {"car" 1},
+                                     ["jumped" "off"] {"The" 1},
+                                     ["The" "car"] {"is" 1},
+                                     ["kid" "jumped"] {"off" 1},
+                                     ["sky" "is"] {"foggy" 1, "sunny" 1, "cloudy" 1, "blue" 1},
+                                     ["is" "sunny"] {"The" 1},
+                                     ["cloudy" "The"] {"sky" 1},
+                                     ["is" "blue"] {"The" 1},
+                                     ["The" "sky"] {"is" 4},
+                                     ["is" "fast"] {"The" 1},
+                                     ["The" "kid"] {"jumped" 1},
+                                     ["fast" "The"] {"sky" 1},
+                                     ["car" "is"] {"fast" 1},
+                                     ["blue" "The"] {"kid" 1},
+                                     ["is" "cloudy"] {"The" 1},
+                                     ["sunny" "The"] {"sky" 1}}
+  (build-database baby-corpus 2) => {["sky"] {"is" 4},
+                                     ["cloudy"] {"The" 1},
+                                     ["kid"] {"jumped" 1},
+                                     ["is"] {"foggy" 1, "sunny" 1, "cloudy" 1, "fast" 1, "blue" 1},
+                                     ["jumped"] {"off" 1},
+                                     ["off"] {"The" 1},
+                                     ["The"] {"car" 1, "kid" 1, "sky" 4},
+                                     ["car"] {"is" 1},
+                                     ["sunny"] {"The" 1},
+                                     ["blue"] {"The" 1},
+                                     ["fast"] {"The" 1}}
+  )
+;; The above data generated using clojure.pprint/pprint
+
+(lookup ["The"] (build-database baby-corpus 2))
+
+(create-sentence "The car" (build-database baby-corpus 3))
