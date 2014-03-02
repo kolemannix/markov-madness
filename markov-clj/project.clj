37bdabad97adfe48b2b21ffddc9f412f7b554eeb
bbaumgar <bwb8ta@virginia.edu>
2014-03-02 08:57:52 -0500

    Add github dependency to project.clj

--- markov-clj/project.clj
+++ markov-clj/project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [midje "1.6.2"]
-                 [twitter-api "0.7.5"]]
+                 [twitter-api "0.7.5"]
+                 [tentacles "0.2.5"]]
   :main markov-clj.twitter)
