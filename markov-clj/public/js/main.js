3334c24916a9ecce7703457522d6a36dcc80f1a2
bbaumgar <bwb8ta@virginia.edu>
2014-03-02 12:00:13 -0500

    Modify Angular example app to fit our needs.

--- markov-clj/public/js/main.js
+++ markov-clj/public/js/main.js
@@ -1,39 +1,28 @@
 /**
  * Created by brent on 3/2/14.
  */
-angular.module('GitHacks', ['ngRoute' ])
-
-    .value('fbURL', 'https://angularjs-projects.firebaseio.com/')
+angular.module('GitHacks', [ ])
 
     .service('clojureService', ['$http', function($http, fbURL) {
-        return $firebase(new Firebase(fbURL));
+        var service = {};
+
+        var url="/";
+
+        service.generateCommits = function(username, cb) {
+            $http.post("/commits", {username: username})
+                .success(cb)
+            .error(function(d) {console.log(d)});
+        }
     }])
 
-    .controller('ListCtrl', function($scope, Projects) {
-        $scope.projects = Projects;
-    })
+    .controller('GitCtrl', ['$scope', 'clojureService', function($scope, clojureService) {
+        $scope.username = "bbaumgar";
 
-    .controller('CreateCtrl', function($scope, $location, $timeout, Projects) {
-        $scope.save = function() {
-            Projects.$add($scope.project, function() {
-                $timeout(function() { $location.path('/'); });
-            });
-        };
-    })
+        $scope.goGoGenerate = function() {
+            clojureService.generateCommits($scope.username, function(d) {
+                console.log(d);
+            })
+        }
 
-    .controller('EditCtrl',
-    function($scope, $location, $routeParams, $firebase, fbURL) {
-        var projectUrl = fbURL + $routeParams.projectId;
-        $scope.project = $firebase(new Firebase(projectUrl));
-
-        $scope.destroy = function() {
-            $scope.project.$remove();
-            $location.path('/');
-        };
-
-        $scope.save = function() {
-            $scope.project.$save();
-            $location.path('/');
-        };
-    });
+    }]);
 
