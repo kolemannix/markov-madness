b92128fe0b0e5bb069449903790d2334f38ffbff
Koleman Nix <kolemannix@gmail.com>
2014-03-05 13:33:41 -0500

    update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject markov-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject markov-clj "1.0.0"
+  :description "HackNC 2014 project"
+  :url "http://brentbaum.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :uberjar-name "markov-madness.jar"
